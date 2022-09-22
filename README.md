1b192e648e860f13bae202c3d446931c9ac65eda
meaux21 <maurice.grimes21@gmail.com>
2022-09-22 13:05:32 -0400

    ReadMe

---
+++ README.md
@@ -0,0 +1,36 @@
+
+<div align="center">
+<h1><strong>Snake</strong> 
+<img src="https://media.giphy.com/media/26DN0U3SqKDG2fTFe/giphy.gif" width="px"/></h1>
+
+
+<div align="center">
+
+## <strong>Description</strong>
+
+Snake is an animated gate that was created in 1997 by Taneli Armanato and first appeared on the Nokia devices. With the evolution of tech the game has been created by multiple authors including myself and I put a personal spin on it. 
+
+### Hope you enjoy!
+
+
+
+## <strong>Images</strong>
+The following photos will show you how the game looks and the controls used for the game.
+
+![Snake Frame](./images/snake.png)
+
+## <strong>Controls</strong>
+![](./images/marcus-urbenz-cUIl28-EWeI-unsplash.jpg)
+## <strong>Tech Stack</strong>
+
+    - JavaScript
+    - HTML
+    - CSS
+
+
+## <strong>Upcoming Features</strong>
+
+    - Allow user to pick a username
+    - Allow user to pick a Color of Snake
+    - Allow user to pick a Level Difficulty
+    - Different styling for background and apple
--- script.js
+++ script.js
@@ -3,24 +3,24 @@ import { drawFood, updateFood} from "./food.js"
 import { outsideGrid } from './grid.js'
 
 // Player Scoreboard Unfinished
-const playerName = document.querySelector('#Name')
-const updateName = document.querySelector("input")
-const startBtn = document.querySelector('#start') 
+// const playerName = document.querySelector('#Name')
+// const updateName = document.querySelector("input")
+// const startBtn = document.querySelector('#start') 
 const resetBtn = document.querySelector('#reset')
 const img = document.createElement('img')
 
 img.src ='./images/snake.jpg'
-console.dir(updateName)
+// console.dir(updateName)
 
-const nameUpdate = () => {
-    // updateName.textContent = updateName.value
-    if (updateName.value === ''){
-        return
-    }
-    playerName.textContent = updateName.value
-    updateName.value = null
-}
-startBtn.addEventListener('click',nameUpdate)
+// const nameUpdate = () => {
+//     // updateName.textContent = updateName.value
+//     if (updateName.value === ''){
+//         return
+//     }
+//     playerName.textContent = updateName.value
+//     updateName.value = null
+// }
+// startBtn.addEventListener('click',nameUpdate)
 
 const reset = () => {
     location.reload()
@@ -48,7 +48,6 @@ function gameLoop(currentTime){
     if(gameOver){
         location.reload()
         // if(confirm('You Lose, Press Okay to try again')){
-            
         // }
         return
     }
